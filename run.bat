9b8af5f166c76e1b99b4d4097f8a719bfa0435c2
phoenix(LyYang) <1903935168@qq.com>
2025-03-24 15:36:59 +0800

    update

--- run.bat
+++ run.bat
@@ -3,6 +3,6 @@ REM 激活 Conda 环境
 call conda activate cosyvoice
 
 REM 运行 Python 脚本
-python webui.py --port 50000 --model_dir pretrained_models/CosyVoice-300M
+python webui.py --port 50000 --model_dir pretrained_models/CosyVoice2-0.5B
 
 pause
